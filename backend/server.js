02f73424ee6a93a2c1b62bc6d968a53e50fb6433
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2023-07-01 00:56:15 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRouter = require('./routers/userRouter')
+const pizzaRouter = require('./routers/pizzaRouter')
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser")
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json());
+dotenv.config();
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/auth', userRouter);
+app.use('/api/plans', pizzaRouter);
+
+app.listen(5000, console.log(`Server is listening to port ${PORT}`))
