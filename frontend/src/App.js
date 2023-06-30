5cb9ee043f5c1a89fda9c6684f4d9234340cb9cf
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2023-07-01 00:41:42 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import AuthPage from './Pages/AuthPage';
+import HomePage from  './Pages/HomePage';
+import MenuPage from './Pages/MenuPage';
+import PrivateRoute from './Components/PrivateAuth/PrivateRoute';
+import AppProvider from './context/AppProvider';
+
+function App() {
+  return (
+    <div className="App">    
+      <Router>
+        <AppProvider>
+          <Routes>
+            <Route path="/" element={<AuthPage />}/>
+            <Route path = "/menu" element ={<PrivateRoute Component={MenuPage}/>}/>
+            <Route path="/home" element={<PrivateRoute Component={HomePage}/>}/>
+          </Routes>
+        </AppProvider>
+      </Router>   
+    </div>
+  );
+}
+
+export default App;
