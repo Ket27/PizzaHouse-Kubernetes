54c7d9232d690e8f4c255c3c050019efacc77775
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2023-07-02 23:31:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const connectDB = require('./config/db');
 const userRouter = require('./routers/userRouter')
 const pizzaRouter = require('./routers/pizzaRouter')
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const reviewRouter = require('./routers/reviewRouter');
 
 app.use(cookieParser());
 app.use(express.json());
@@ -16,5 +17,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', userRouter);
 app.use('/api/plans', pizzaRouter);
+app.use('/api/reviews', reviewRouter)
 
 app.listen(5000, console.log(`Server is listening to port ${PORT}`))
